48a8fd9e488214ef8e9ce57a75f6d9206eb194d9
yihu55 <veronicayi@msn.com>
2022-03-28 10:24:40 +0200

    change server porto to 5000

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 
-const app = express()
-const PORT = 3000;
+const app = express();
+const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.get('/', (req, res) => {
+  res.send('Hello world!');
 });
 
 app.listen(PORT, () => {
