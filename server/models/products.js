573f2c0859f5741f3bb04dc9ec8af37d07343c1c
yihu55 <veronicayi@msn.com>
2022-03-28 14:04:09 +0200

    add mongodb

--- server/models/products.js
+++ server/models/products.js
@@ -1,14 +1,57 @@
-const fs = require('fs');
-const path = require('path');
-const util = require('util');
+// const fs = require('fs');
+// const path = require('path');
+// const util = require('util');
 
-//return a promise
-const readFile = util.promisify(fs.readFile);
+// //return a promise
+// const readFile = util.promisify(fs.readFile);
 
-const readProducts = async () => {
-  const productsFile = path.join(__dirname, 'products.json');
-  const productJson = await readFile(productsFile);
-  return JSON.parse(productJson);
+// const readProducts = async () => {
+//   const productsFile = path.join(__dirname, 'products.json');
+//   const productJson = await readFile(productsFile);
+//   return JSON.parse(productJson);
+// };
+
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  sku: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  discountPrice: Number,
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+const Product = mongoose.model('Product', productSchema);
+
+const getAllProducts = async () => {
+  return await Product.find();
 };
-
-module.exports = { readProducts };
+const getProduct = async (sku) => {
+  return await Product.findOne({ sku });
+};
+//return a lista
+// const getAllProducts = async () => {
+//   const { products } = await readProducts();
+//   return products;
+// };
+// const getProduct = async (sku) => {
+//   const { products } = await readProducts();
+//   return products.find(({ product }) => product.sku === sku);
+// };
+//module.exports = { readProducts };
+module.exports = { getAllProducts, getProduct };
